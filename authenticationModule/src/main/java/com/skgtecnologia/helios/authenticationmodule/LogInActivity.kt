5209e59bed4e39491a1374c8396992cb26610119
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-08-22 11:22:57 -0500

    refactor: Set the login screen to primary entry point

--- authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/LogInActivity.kt
+++ authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/LogInActivity.kt
@@ -7,11 +7,8 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import com.skgtecnologia.helios.authenticationmodule.domain.AuthenticationTokenExchange
 import com.skgtecnologia.helios.authenticationmodule.domain.Router
@@ -25,12 +22,10 @@ class LogInActivity : ComponentActivity() {
         val loginIntent: Intent by inject()
         val authenticationTokenExchange: AuthenticationTokenExchange by inject()
         val authenticationRouter: Router by inject()
-        val showHelperScreen = mutableStateOf(false)
         val getResult = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult(),
         ) {
             if (it.resultCode == Activity.RESULT_OK) {
-                showHelperScreen.value = false
                 Toast.makeText(this, "Authentication DONE!!", Toast.LENGTH_LONG).show()
                 val value = it.data!!
                 authenticationTokenExchange.requestToken(value) {
@@ -38,18 +33,12 @@ class LogInActivity : ComponentActivity() {
                 }
             } else {
                 Toast.makeText(this, "Authentication FAILURE!!", Toast.LENGTH_LONG).show()
-                showHelperScreen.value = true
             }
         }
 
         setContent {
-            LaunchedEffect(key1 = Unit, block = {
+            SecondaryLogInScreen() {
                 getResult.launch(loginIntent)
-            })
-            AnimatedVisibility(visible = showHelperScreen.value) {
-                SecondaryLogInScreen() {
-                    getResult.launch(loginIntent)
-                }
             }
         }
     }
