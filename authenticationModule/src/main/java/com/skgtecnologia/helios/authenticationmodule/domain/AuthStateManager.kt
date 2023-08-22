6d9de27fccb3f5ca0b2520db2e9a8c160cb8056f
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-08-22 08:51:46 -0500

    refactor: delete unused jwt converter

--- authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/domain/AuthStateManager.kt
+++ authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/domain/AuthStateManager.kt
@@ -2,8 +2,6 @@ package com.skgtecnologia.helios.authenticationmodule.domain
 
 import android.app.Application
 import android.content.Context
-import android.text.TextUtils
-import com.auth0.android.jwt.JWT
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -49,10 +47,6 @@ class AuthStateManager(
             try {
                 authState = AuthState.jsonDeserialize(jsonString)
                 _currentToken.value = authState.accessToken ?: "No found"
-// TODO: Handle this part
-                if (!TextUtils.isEmpty(authState.idToken)) {
-                    val jwt = JWT(authState.idToken!!)
-                }
             } catch (_: JSONException) {
             }
         }
