d99a083fa5e57af8c7ecb474bb4a44d39886d420
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-08-25 11:24:35 -0500

    feature: add loading screen

--- authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/LogInActivity.kt
+++ authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/LogInActivity.kt
@@ -7,10 +7,18 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import com.skgtecnologia.helios.authenticationmodule.domain.AuthenticationTokenExchange
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.skgtecnologia.helios.authenticationmodule.domain.AuthenticationRouter
+import com.skgtecnologia.helios.authenticationmodule.domain.AuthenticationTokenExchange
 import com.skgtecnologia.helios.authenticationmodule.presentation.LogInScreen
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import org.koin.android.ext.android.inject
 
 class LogInActivity : ComponentActivity() {
@@ -20,10 +28,12 @@ class LogInActivity : ComponentActivity() {
         val loginIntent: Intent by inject()
         val authenticationTokenExchange: AuthenticationTokenExchange by inject()
         val authenticationRouter: AuthenticationRouter by inject()
+        val loadingFlow = MutableStateFlow(false)
         val getResult = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult(),
         ) {
             if (it.resultCode == Activity.RESULT_OK) {
+                loadingFlow.update { true }
                 val value = it.data!!
                 authenticationTokenExchange.requestToken(value) {
                     authenticationRouter.onUserAuthenticated()
@@ -34,8 +44,21 @@ class LogInActivity : ComponentActivity() {
         }
 
         setContent {
-            SecondaryLogInScreen() {
-                getResult.launch(loginIntent)
+            val loading = loadingFlow.collectAsState()
+            if (loading.value) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxSize(0.1f),
+                    )
+                }
+            } else {
+                SecondaryLogInScreen() {
+                    getResult.launch(loginIntent)
+                }
             }
         }
     }
