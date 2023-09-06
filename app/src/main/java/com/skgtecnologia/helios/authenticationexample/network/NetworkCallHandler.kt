188ea8eb832f29de70e2fab203314f76bf014fe8
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-09-06 10:10:44 -0500

    feature: add check for token refresh

--- app/src/main/java/com/skgtecnologia/helios/authenticationexample/network/NetworkCallHandler.kt
+++ app/src/main/java/com/skgtecnologia/helios/authenticationexample/network/NetworkCallHandler.kt
@@ -1,6 +1,7 @@
 package com.skgtecnologia.helios.authenticationexample.network
 
 import com.skgtecnologia.helios.authenticationmodule.domain.AuthStateManager
+import com.skgtecnologia.helios.authenticationmodule.domain.AuthenticationTokenExchange
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -12,6 +13,7 @@ import java.net.UnknownHostException
 class NetworkCallHandler(
     private val authStateManager: AuthStateManager,
     private val authorizationService: AuthorizationService,
+    private val authenticationTokenExchange: AuthenticationTokenExchange,
 ) {
     fun <T> makeNetworkCall(
         call: suspend () -> T,
@@ -19,12 +21,14 @@ class NetworkCallHandler(
         emit(true) // Emit loading status
         try {
             CoroutineScope(Dispatchers.IO).launch { // TODO: Change with a viewModel scope
-                val authState = getCurrentAuthenticationState()
                 val tokenRefresh = async(Dispatchers.IO) {
-                    authState.performActionWithFreshTokens(
-                        authorizationService,
-                    ) { accessToken, idToken, exception ->
-                        ApiNetworkInterceptor.setCurrentToken(accessToken)
+                    authenticationTokenExchange.refreshToken() {
+                        val authState = getCurrentAuthenticationState()
+                        authState.performActionWithFreshTokens(
+                            authorizationService,
+                        ) { accessToken, idToken, exception ->
+                            ApiNetworkInterceptor.setCurrentToken(accessToken)
+                        }
                     }
                 }
                 // TODO: Review the working scope of this part
