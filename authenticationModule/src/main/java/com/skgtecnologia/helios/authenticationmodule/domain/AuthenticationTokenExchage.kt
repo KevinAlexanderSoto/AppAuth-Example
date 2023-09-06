3aa33d1cab8d64ee3986b835323b92d4c8fd4ac2
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-09-06 10:10:26 -0500

    feature: add function to handle the token refresh

--- authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/domain/AuthenticationTokenExchage.kt
+++ authenticationModule/src/main/java/com/skgtecnologia/helios/authenticationmodule/domain/AuthenticationTokenExchage.kt
@@ -1,14 +1,12 @@
 package com.skgtecnologia.helios.authenticationmodule.domain
 
 import android.content.Intent
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import net.openid.appauth.AuthState
 import net.openid.appauth.AuthorizationException
 import net.openid.appauth.AuthorizationResponse
 import net.openid.appauth.AuthorizationService
+
 /**
  * This is a class called AuthenticationTokenExchange that is responsible for exchanging an authorization code for an access token. It takes in an AuthorizationService and an AuthStateManager as dependencies.
  *
@@ -49,6 +47,25 @@ class AuthenticationTokenExchange(
                     }
                 }
             }
+        }
+    }
+
+    fun refreshToken(onTokenUpdated: () -> Unit) {
+        val authState = authStateManager.restoreState()
+        if (authState.needsTokenRefresh) {
+            authorizationService.performTokenRequest(authState.createTokenRefreshRequest()) { response, exception ->
+                if (exception != null) {
+                    // TODO: Handle exception
+                } else {
+                    if (response != null) {
+                        authState.update(response, exception)
+                        authStateManager.persistState(authState)
+                        onTokenUpdated.invoke()
+                    }
+                }
+            }
+        } else {
+            onTokenUpdated.invoke()
         }
     }
 }
